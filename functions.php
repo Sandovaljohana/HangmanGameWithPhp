60f0ef1fa360732f4c86b78164194220a6c04225
Johana Sandoval <149192818+Sandovaljohana@users.noreply.github.com>
2024-01-21 16:17:08 +0000

    change some styles

--- functions.php
+++ functions.php
@@ -7,19 +7,17 @@ function getCurrentPicture($part){
 function startGame(){
     session_destroy();
     session_start();
-    initializeParts(); // Inicializa las partes
-    $_SESSION["current_part"] = getCurrentPart(); // Inicializa con la primera parte
+    initializeParts();
+    $_SESSION["current_part"] = getCurrentPart(); 
 }
 
-// restart the game. Clear the session variables
 function restartGame(){
     session_destroy();
     session_start();
-    initializeParts(); // Inicializa las partes
-    $_SESSION["current_part"] = getCurrentPart(); // Reinicia con la primera parte
+    initializeParts(); 
+    $_SESSION["current_part"] = getCurrentPart();
 }
 
-// Get all the hangman Parts
 function getParts(){
     return isset($_SESSION["parts"]) ? $_SESSION["parts"] : initializeParts();
 }
@@ -27,7 +25,7 @@ function getParts(){
 function initializeParts(){
     global $bodyParts;
     $_SESSION["parts"] = $bodyParts;
-    $_SESSION["current_part"] = "full"; // Asegurar que la parte actual sea "full" al inicio
+    $_SESSION["current_part"] = "full";
     $_SESSION["word"] = getRandomWord();
     $_SESSION["responses"] = [];
     $_SESSION["gamecomplete"] = false;
@@ -39,25 +37,20 @@ function getRandomWord(){
     return $words[$key];
 }
 
-// add part to the Hangman
 function addPart(){
     $parts = getParts();
     if (!isBodyComplete()) {
         array_shift($parts);
         $_SESSION["parts"] = $parts;
-        $_SESSION["current_part"] = $parts[0]; // Actualizar la parte actual
+        $_SESSION["current_part"] = $parts[0];
     }
 }
 
-// get Current Hangman Body part
 function getCurrentPart(){
     $parts = getParts();
     return $_SESSION["current_part"];
 }
 
-// user responses logic
-
-// get user response
 function getCurrentResponses(){
     return isset($_SESSION["responses"]) ? $_SESSION["responses"] : [];
 }
@@ -68,29 +61,22 @@ function addResponse($letter){
     $_SESSION["responses"] = $responses;
 }
 
-// check if pressed letter is correct
 function isLetterCorrect($letter){
     $word = $_SESSION["word"];
     return strpos($word, $letter) !== false;
 }
 
-// is the word (guess) correct
 function isWordCorrect(){
     $guess = $_SESSION["word"];
     $responses = getCurrentResponses();
     return str_replace($responses, '', $guess) === '';
 }
 
-// check if the body is ready to hang
 function isBodyComplete(){
     $parts = getParts();
-    // is the current parts less than or equal to one
     return count($parts) <= 1;
 }
 
-// manage game session
-
-// is game complete
 function gameComplete(){
     if (isWordCorrect() && !isBodyComplete()) {
         return "WON";
@@ -100,39 +86,33 @@ function gameComplete(){
     return false;
 }
 
-// set game as complete
 function markGameAsComplete(){
     $_SESSION["gamecomplete"] = true;
 }
 
-// start a new game
 function markGameAsNew(){
     $_SESSION["gamecomplete"] = false;
 }
 
-/* Detect when the game is to restart. From the restart button press */
 if(isset($_GET['start'])){
     restartGame();
 }
 
-/* Detect when Key is pressed */
 if(isset($_GET['kp'])){
     $currentPressedKey = isset($_GET['kp']) ? $_GET['kp'] : null;
-    // if the key press is correct
     if($currentPressedKey && isLetterCorrect($currentPressedKey) && !isBodyComplete() && !gameComplete()){
         addResponse($currentPressedKey);
         if(isWordCorrect()){
             markGameAsComplete();
         }
     } else {
-        // start hanging the man :)
         if(!isBodyComplete()){
             addPart(); 
             if(isBodyComplete()){
-                markGameAsComplete(); // lost condition
+                markGameAsComplete();
             }
         } else {
-            markGameAsComplete(); // lost condition
+            markGameAsComplete(); 
         }
     }
 }
--- index.php
+++ index.php
@@ -1,71 +1,76 @@
-
-<?php session_start();?>
-<?php include 'functions.php' ; ?>
-<?php include 'config.php' ; ?>
+<?php session_start(); ?>
+<?php include 'functions.php'; ?>
+<?php include 'config.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hangman game</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sofia&display=swap" rel="stylesheet">
+    <title>Ahorcado</title>
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body class="background">
 
-<div class="hangman-container">
-    <div class="hangman-image">
-        <img src="<?php echo getCurrentPicture(getCurrentPart());?>" alt="Hangman Image"/>
-        <?php $gameResult = gameComplete(); ?>
-        <?php if ($gameResult): ?>
-            <h1>GAME COMPLETE</h1>
-            <?php if ($gameResult === "WON"): ?>
-                <p class="game-won">You Won! HURRAY! :)</p>
-            <?php elseif ($gameResult === "LOST"): ?>
-                <p class="game-lost">You LOST! OH NO! :(</p>
+    <div class="hangman-container">
+        <div class="hangman-image">
+            <img src="<?php echo getCurrentPicture(getCurrentPart()); ?>" alt="Hangman Image" />
+            <?php $gameResult = gameComplete(); ?>
+            <?php if ($gameResult) : ?>
+                <h1></h1>
+                <?php if ($gameResult === "WON") : ?>
+                    <p class="game-won">Ganaste!</p>
+                <?php elseif ($gameResult === "LOST") : ?>
+                    <p class="game-lost">Perdiste! Inténtalo de nuevo!</p>
+                <?php endif; ?>
             <?php endif; ?>
-        <?php endif; ?>
-    </div>
+        </div>
 
-    <div class="hangman-info">
-        <h1>Hangman the Game</h1>
-        <div class="letter-buttons">
-            <form method="get">
-                <?php
-                $max = strlen($letters) - 1;
-                for ($i = 0; $i <= $max; $i++) :
-                    ?>
-                    <button type='submit' name='kp' value='<?php echo $letters[$i]; ?>'>
-                        <?php echo $letters[$i]; ?>
-                    </button>
+        <div class="hangman-info">
+            <h1>Juego del Ahorcado</h1>
+            <div class="letter-buttons">
+                <form method="get">
                     <?php
-                    if ($i % 7 == 0 && $i > 0) {
-                        echo '<br>';
-                    }
-                endfor;
-                ?>
-                <br><br>
-                <button type="submit" name="start">Restart Game</button>
-            </form>
+                    $max = strlen($letters) - 1;
+                    for ($i = 0; $i <= $max; $i++) :
+                    ?>
+                        <button type='submit' name='kp' value='<?php echo $letters[$i]; ?>'>
+                            <?php echo $letters[$i]; ?>
+                        </button>
+                    <?php
+                        if ($i % 7 == 0 && $i > 0) {
+                            echo '<br>';
+                        }
+                    endfor;
+                    ?>
+                    <br><br>
+                    <button type="submit" name="start">Empezar de nuevo</button>
+                </form>
+            </div>
+        </div>
+
+        <div class="hangman-guess">
+            <?php
+            $guess = $_SESSION["word"];
+            $responses = getCurrentResponses();
+            $maxLetters = strlen($guess) - 1;
+            for ($j = 0; $j <= $maxLetters; $j++) :
+                $l = $guess[$j];
+            ?>
+                <?php if (in_array($l, $responses)) : ?>
+                    <span class="guessed-letter"><?php echo $l; ?></span>
+                <?php else : ?>
+                    <span class="unguessed-letter">&nbsp;&nbsp;&nbsp;</span>
+                <?php endif; ?>
+            <?php endfor; ?>
         </div>
     </div>
 
-    <div class="hangman-guess">
-        <?php
-        $guess = $_SESSION["word"];
-        $responses = getCurrentResponses();
-        $maxLetters = strlen($guess) - 1;
-        for ($j = 0; $j <= $maxLetters; $j++) :
-            $l = $guess[$j];
-            ?>
-            <?php if (in_array($l, $responses)): ?>
-                <span class="guessed-letter"><?php echo $l; ?></span>
-            <?php else: ?>
-                <span class="unguessed-letter">&nbsp;&nbsp;&nbsp;</span>
-            <?php endif; ?>
-        <?php endfor; ?>
-    </div>
-</div>
-
 </body>
+
 </html>
